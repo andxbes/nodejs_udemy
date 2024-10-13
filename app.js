3572e8d24988c2d3ced4d809b5da1efb9d4c34c4
andxbes <andxbes@gmail.com>
2024-10-13 19:54:16 +0300

    94

--- app.js
+++ app.js
@@ -6,15 +6,8 @@ const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine(
-  'hbs',
-  expressHbs({
-    layoutsDir: 'views/layouts/',
-    defaultLayout: 'main-layout',
-    extname: 'hbs'
-  })
-);
-app.set('view engine', 'hbs');
+
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -27,7 +20,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+    res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
