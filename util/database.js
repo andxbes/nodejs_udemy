d75033c26eb1647dd1a2d4a7ec9347afd6ca7eb3
andxbes <andxbes@gmail.com>
2024-10-20 17:15:47 +0300

    fix admin

--- util/database.js
+++ util/database.js
@@ -1,13 +1,6 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-console.info({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USERNAME,
-    database: process.env.DATABASE_NAME,
-    password: process.env.DATABASE_PASSWORD
-});
-
 const pool = mysql.createPool({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USERNAME,
