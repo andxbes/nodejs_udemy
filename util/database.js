879b2b1829211709133c5b0350b305d2d3422e3c
andxbes <andxbes@gmail.com>
2024-10-20 22:56:42 +0300

    176

--- util/database.js
+++ util/database.js
@@ -1,11 +1,26 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 require('dotenv').config();
 
-const pool = mysql.createPool({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USERNAME,
-    database: process.env.DATABASE_NAME,
-    password: process.env.DATABASE_PASSWORD
-});
+// const pool = mysql.createPool({
+//     host: process.env.DATABASE_HOST,
+//     user: process.env.DATABASE_USERNAME,
+//     database: process.env.DATABASE_NAME,
+//     password: process.env.DATABASE_PASSWORD
+// });
 
-module.exports = pool.promise();
+// module.exports = pool.promise();
+
+const { Sequelize } = require('sequelize');
+
+
+const sequelize = new Sequelize(
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USERNAME,
+  process.env.DATABASE_PASSWORD,
+  {
+    dialect: 'mysql',
+    host: process.env.DATABASE_HOST
+  }
+);
+
+module.exports = sequelize;
