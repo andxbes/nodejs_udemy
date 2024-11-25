3bb94b018169c588573e8c0d7063792bd58c3bbd
andxbes <andxbes@gmail.com>
2024-11-25 22:12:57 +0200

    277

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,45 @@
+require("dotenv").config();
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
+
+const oauth2Client = new OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  "https://developers.google.com/oauthplayground"
+);
+
+oauth2Client.setCredentials({
+  refresh_token:
+    process.env.REFRESH_TOKEN,
+});
+
+const accessToken = oauth2Client.getAccessToken();
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    type: "OAuth2",
+    user: process.env.USER_EMAIL_SENDER,
+    clientId:
+      process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken:
+      process.env.REFRESH_TOKEN,
+    accessToken,
+  },
+});
+
+
+transporter.sendMail({
+  from: process.env.USER_EMAIL_SENDER,
+  to: process.env.USER_EMAIL_SENDER,
+  subject: "Sending email using Nodemailer and OAuth 2.0",
+  text: "Sent!",
+});
 
 const User = require('../models/user');
 
@@ -68,7 +109,10 @@ exports.postSignup = (req, res, next) => {
   User.findOne({ email: email })
     .then(userDoc => {
       if (userDoc) {
-        req.flash('error', 'E-Mail exists already, please pick a different one.');
+        req.flash(
+          'error',
+          'E-Mail exists already, please pick a different one.'
+        );
         return res.redirect('/signup');
       }
       return bcrypt
@@ -83,6 +127,15 @@ exports.postSignup = (req, res, next) => {
         })
         .then(result => {
           res.redirect('/login');
+          return transporter.sendMail({
+            to: email,
+            from: 'shop@node-complete.com',
+            subject: 'Signup succeeded!',
+            html: '<h1>You successfully signed up!</h1>'
+          });
+        })
+        .catch(err => {
+          console.log(err);
         });
     })
     .catch(err => {
@@ -94,5 +147,19 @@ exports.postLogout = (req, res, next) => {
   req.session.destroy(err => {
     console.log(err);
     res.redirect('/');
+  });
+};
+
+exports.getReset = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render('auth/reset', {
+    path: '/reset',
+    pageTitle: 'Reset Password',
+    errorMessage: message
   });
 };
