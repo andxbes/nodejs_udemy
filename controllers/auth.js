08f1d84e840325b8b9a2f9fc2ef986f1a5c5be14
andxbes <andxbes@gmail.com>
2024-11-26 18:44:32 +0200

    fix sending email

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,42 +1,20 @@
 require("dotenv").config();
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
-const { google } = require("googleapis");
-const OAuth2 = google.auth.OAuth2;
 
-const oauth2Client = new OAuth2(
-  process.env.CLIENT_ID,
-  process.env.CLIENT_SECRET,
-  "https://developers.google.com/oauthplayground"
-);
-
-oauth2Client.setCredentials({
-  refresh_token:
-    process.env.REFRESH_TOKEN,
-});
-
-const accessToken = oauth2Client.getAccessToken();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  service: 'gmail',
   auth: {
-    type: "OAuth2",
-    user: process.env.USER_EMAIL_SENDER,
-    clientId:
-      process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    refreshToken:
-      process.env.REFRESH_TOKEN,
-    accessToken,
-  },
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD
+  }
 });
 
 
 transporter.sendMail({
-  from: process.env.USER_EMAIL_SENDER,
-  to: process.env.USER_EMAIL_SENDER,
+  from: process.env.EMAIL_USER,
+  to: 'meyakal284@exoular.com',
   subject: "Sending email using Nodemailer and OAuth 2.0",
   text: "Sent!",
 });
